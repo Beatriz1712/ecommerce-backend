ea863a881f39f75e79d24ca4e81bc1afe059a938
beatriz1712 <beatriz1712sc@gmail.com>
2023-11-27 20:45:34 -0500

    subiendo nuevamente el repo

---
+++ src/router/views.routes.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const ViewsRouter = express.Router()
+
+//Rutas GET para la página de inicio y detalles del producto:
+
+ViewsRouter.get("/inicio", async (req, res) => {
+    res.render("inicio", {
+        title: "App de compras",
+    })
+})
+ViewsRouter.get("/register", (req, res) => {
+    res.render("register", {
+        title: "Registro de Usuario"
+    })
+})
+
+ViewsRouter.get("/login", (req, res) => {
+    res.render("login", {
+        title: "Login de Usuario"
+    })
+})
+
+export default ViewsRouter
