3589f21b0fd0ccefcf80e661112c6db497c82eae
beatriz1712 <beatriz1712sc@gmail.com>
2023-12-08 21:28:32 -0500

    iniciando tercera entrega

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,16 @@
 import path from "path"
 import { fileURLToPath } from "url"
-import bcrypt, { genSaltSync } from "bcrypt"
+import bcrypt from "bcrypt"
 
 export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password)
+export const isValidPassword = async (user, password) => {
+    try {
+        return await bcrypt.compare(password, user.password)
+    } catch (error)
+{
+    throw new Error('error al comparar contraseñas');
+}
+}
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
